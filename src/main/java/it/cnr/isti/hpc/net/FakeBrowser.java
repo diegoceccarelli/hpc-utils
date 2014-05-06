55f0f455869f542d1705a6a5a528ebb36ff23a4b
diegoceccarelli <diego.ceccarelli@gmail.com>
2014-05-06 11:25:59 +0200

    add utf8 read in fakebrowser

--- src/main/java/it/cnr/isti/hpc/net/FakeBrowser.java
+++ src/main/java/it/cnr/isti/hpc/net/FakeBrowser.java
@@ -25,83 +25,138 @@ import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Map;
 
-
 /**
  * Simulates a Firefox browser in order to send queries to google/wikipedia...
  * 
  * @author Diego Ceccarelli, diego.ceccarelli@isti.cnr.it created on 06/giu/2012
  */
 public class FakeBrowser {
-	
-	private Map<String,String> properties;
-	
-	
-	public FakeBrowser(){
+
+	private final Map<String, String> properties;
+
+	public FakeBrowser() {
 		properties = new HashMap<String, String>();
-		properties.put("User-Agent", "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11");
+		properties
+				.put("User-Agent",
+						"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11");
 	}
-	
 
 	/**
 	 * Fetches the url and returns a reader with the content of the page.
-	 * @param url the url to fetch.
+	 * 
+	 * @param url
+	 *            the url to fetch.
 	 * @return a reader to the page of the url.
-	 * @throws IOException if an errors occurs during the fetching.
+	 * @throws IOException
+	 *             if an errors occurs during the fetching.
 	 */
 	public Reader fetch(URL url) throws IOException {
 		URLConnection urlc;
 		urlc = url.openConnection();
-		for (Map.Entry<String, String> entry : properties.entrySet()){
+		for (Map.Entry<String, String> entry : properties.entrySet()) {
 			urlc.setRequestProperty(entry.getKey(), entry.getValue());
 		}
-		
+
 		InputStream is = urlc.getInputStream();
 		BufferedReader br = new BufferedReader(new InputStreamReader(is));
 		return br;
 	}
-	
-	/**
-	 * Adds a property to the request (e.g., a cookie..) 
-	 * @param name the name fo the property
-	 * @param value the value of the property
-	 */
-	public void addProperty(String name, String value){
-		properties.put(name, value);
-	}
-	
-	
+
 	/**
 	 * Fetches the url and returns a reader with the content of the page.
-	 * @param url the url to fetch.
+	 * 
+	 * @param url
+	 *            the url to fetch.
 	 * @return a reader to the page of the url.
-	 * @throws IOException if an errors occurs during the fetching.
+	 * @throws IOException
+	 *             if an errors occurs during the fetching.
+	 */
+	public Reader fetchUTF8(URL url) throws IOException {
+		URLConnection urlc;
+		urlc = url.openConnection();
+		for (Map.Entry<String, String> entry : properties.entrySet()) {
+			urlc.setRequestProperty(entry.getKey(), entry.getValue());
+		}
+
+		InputStream is = urlc.getInputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is,
+				"UTF-8"));
+		return br;
+	}
+
+	/**
+	 * Adds a property to the request (e.g., a cookie..)
+	 * 
+	 * @param name
+	 *            the name fo the property
+	 * @param value
+	 *            the value of the property
+	 */
+	public void addProperty(String name, String value) {
+		properties.put(name, value);
+	}
+
+	/**
+	 * Fetches the url and returns a reader with the content of the page.
+	 * 
+	 * @param url
+	 *            the url to fetch.
+	 * @return a reader to the page of the url.
+	 * @throws IOException
+	 *             if an errors occurs during the fetching.
 	 */
 	public Reader fetch(String url) throws IOException {
 		return fetch(new URL(url));
 	}
-	
-	
+
+	/**
+	 * Fetches the url and returns a reader with the content of the page.
+	 * 
+	 * @param url
+	 *            the url to fetch.
+	 * @return a reader to the page of the url.
+	 * @throws IOException
+	 *             if an errors occurs during the fetching.
+	 */
+	public Reader fetchUTF8(String url) throws IOException {
+		return fetch(new URL(url));
+	}
+
 	/**
 	 * Fetches the url and returns a string with the content of the page.
-	 * @param url the url to fetch.
+	 * 
+	 * @param url
+	 *            the url to fetch.
 	 * @return string containing the content of the page.
-	 * @throws IOException if an errors occurs during the fetching.
+	 * @throws IOException
+	 *             if an errors occurs during the fetching.
 	 */
-	public StringBuilder fetchAsString(String url) throws IOException{
+	public String fetchAsString(String url) throws IOException {
 		return asString(fetch(url));
 	}
-	
-	
-	private StringBuilder asString(Reader r) throws IOException {
+
+	/**
+	 * Fetches the url and returns a string with the content of the page.
+	 * 
+	 * @param url
+	 *            the url to fetch.
+	 * @return string containing the content of the page.
+	 * @throws IOException
+	 *             if an errors occurs during the fetching.
+	 */
+	public String fetchAsUTF8String(String url) throws IOException {
+		return asString(fetchUTF8(url));
+	}
+
+	private String asString(Reader r) throws IOException {
 		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(r);
 		String line = "";
 		while ((line = br.readLine()) != null) {
 			sb.append(line).append(" ");
 		}
-		return sb;
+		return sb.toString();
 
 	}
-
 
 }
